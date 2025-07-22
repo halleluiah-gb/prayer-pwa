bb82dc086be2783be3dc11c83ed180debcf6215f
halleluiah-gb <bijyjiby@gmail.com>
2025-07-22 11:48:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,12 +10,23 @@ const urlsToCache = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => {
+        return new Response("⚠️ You're offline and this resource is not cached.");
+      });
+    })
   );
 });
